0900c3c3fc5b3bb104c118c5088238f79fbb56ee
Chisadin <tranthanhnam.ccc@gmail.com>
2026-03-22 23:04:21 +0700

    feat(cron): Implement cronjob for checking new transaction

--- payment/src/index.ts
+++ payment/src/index.ts
@@ -1,13 +1,28 @@
 import { Elysia } from "elysia";
 import { get_data } from "./module/mbbank";
 import { config } from "dotenv";
+import { startCron, stopCron } from "./module/cron";
 config();
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
 const USERNAME = process.env.MB_USERNAME;
 const PASSWORD = process.env.MB_PASSWORD;
 const ACCOUNT_NUMBER = process.env.MB_ACCOUNT;
-const WEBHOOK_URL = process.env.WEBHOOK_URL;
+const WEBHOOK_URL = process.env.WEBHOOK_SERVICE;
+const HMAC = process.env.HMAC;
+
+const app = new Elysia().get("/", () => "Hello Elysia").onStart(async ({server}) => {
+  if (!USERNAME || !PASSWORD || !ACCOUNT_NUMBER || !WEBHOOK_URL || !HMAC) {
+    console.error("MB_USERNAME, MB_PASSWORD, MB_ACCOUNT, WEBHOOK_URL, and HMAC must be defined in environment variables.");
+    console.error(`Current values: USERNAME=${USERNAME}, PASSWORD=${PASSWORD ? "******" : undefined}, ACCOUNT_NUMBER=${ACCOUNT_NUMBER}, WEBHOOK_URL=${WEBHOOK_URL}, HMAC=${HMAC ? "******" : undefined}`);
+    return;
+  }
+  console.log(`Elysia server is starting at ${server?.hostname}:${server?.port}...`);
+  console.log("Elysia server has started. Starting cron job to check for new transactions...");
+  startCron(USERNAME, PASSWORD, ACCOUNT_NUMBER);
+}).onStop(() => {
+  console.log("Elysia server is stopping...");
+  stopCron();
+}).listen(3000);
 
 app.get("/check", async () => {
   
@@ -24,10 +39,9 @@ app.get("/check", async () => {
   }
 })
 
-app.on("start", async () => {
-  // Startup event ở đây
 
-})
+
+
 
 
 console.log(
